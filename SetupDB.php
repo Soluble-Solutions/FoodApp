18591e8fea6f8a688d018b2370c3aef918128685
EttaMariG <ettamari@hotmail.com>
2016-03-23 19:40:12 -0500

    Insert statements are up and working

--- SetupDB.php
+++ SetupDB.php
@@ -21,7 +21,7 @@ $dbname = "foodapp";
 
   $sql = 'CREATE TABLE IF NOT EXISTS Attribute
   (
-    attribute_id INT  NOT NULL  AUTO_INCREMENT,
+    attribute_id INT  NOT NULL,
     name VARCHAR(45)  NOT NULL,
     CONSTRAINT Attribute_pk PRIMARY KEY (attribute_id)
   );
@@ -34,7 +34,6 @@ $dbname = "foodapp";
     time_stamp DateTime  NOT NULL,
     image VARCHAR(100)  NOT NULL,
     dh_id INT NOT NULL,
-    comment_id INT NOT NULL,
     station_id INT  NOT NULL,
     CONSTRAINT entry_id PRIMARY KEY (entry_id)
   );
@@ -51,14 +50,14 @@ $dbname = "foodapp";
 
   CREATE TABLE IF NOT EXISTS Dining_Hall
   (
-    dh_id INT  NOT NULL  AUTO_INCREMENT,
+    dh_id INT  NOT NULL,
     name VARCHAR(45)  NOT NULL,
     CONSTRAINT Dining_Hall_pk PRIMARY KEY (dh_id)
   );
 
   CREATE TABLE IF NOT EXISTS Entry_Attributes
   (
-    entry_id INT  NOT NULL  AUTO_INCREMENT,
+    entry_id INT  NOT NULL,
     attribute_id INT  NOT NULL,
     CONSTRAINT Entry_Attributes_pk PRIMARY KEY (entry_id,attribute_id),
     CONSTRAINT Entry_Attributes_Attribute_fk FOREIGN KEY (attribute_id) REFERENCES Attribute (attribute_id),
@@ -67,7 +66,7 @@ $dbname = "foodapp";
 
   CREATE TABLE IF NOT EXISTS Station
   (
-    station_id INT  NOT NULL  AUTO_INCREMENT,
+    station_id INT  NOT NULL,
     name VARCHAR(45)  NOT NULL,
     CONSTRAINT Station_pk PRIMARY KEY (station_id)
   );
@@ -84,74 +83,66 @@ $dbname = "foodapp";
   $conn->query($sql);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $sql = "insert into Entry (title, votes, time_stamp, image, dh_id, comment_id, station_id) values ('aliquet', 76, '2010-07-14 19:00', 'http:/dummyimage.com/183x191.gif/ff4444/ffffff', 1, 1, 4);
-  insert into Entry (title, votes, time_stamp, image, dh_id, comment_id, station_id) values ('purus', 38, '2008-05-14 20:00', 'http:/dummyimage.com/184x163.png/cc0000/ffffff', 2, 2, 12);
-insert into Entry (title, votes, time_stamp, image, dh_id, comment_id, station_id) values ('in', 91, '2012-05-14 08:00', 'http:/dummyimage.com/158x103.jpg/5fa2dd/ffffff', 1, 3, 1);
-insert into Entry (title, votes, time_stamp, image, dh_id, comment_id, station_id) values ('etiam', 61, '2009-12-14 17:00', 'http:/dummyimage.com/159x243.png/cc0000/ffffff', 2, 4, 5);
-insert into Entry_Attributes (entry_id, attribute_id) values (1, 1);
-insert into Entry_Attributes (entry_id, attribute_id) values (2, 1);
-insert into Entry_Attributes (entry_id, attribute_id) values (3, 1);
-insert into Entry_Attributes (entry_id, attribute_id) values (4, 1);
-insert into Entry_Attributes (entry_id, attribute_id) values (1, 2);
-insert into Entry_Attributes (entry_id, attribute_id) values (2, 2);
-insert into Entry_Attributes (entry_id, attribute_id) values (3, 2);
-insert into Entry_Attributes (entry_id, attribute_id) values (4, 2);
-insert into Entry_Attributes (entry_id, attribute_id) values (1, 3);
-insert into Entry_Attributes (entry_id, attribute_id) values (2, 3);
-insert into Entry_Attributes (entry_id, attribute_id) values (3, 3);
-insert into Entry_Attributes (entry_id, attribute_id) values (4, 3);
-insert into Entry_Attributes (entry_id, attribute_id) values (1, 4);
-insert into Entry_Attributes (entry_id, attribute_id) values (2, 4);
-insert into Entry_Attributes (entry_id, attribute_id) values (3, 4);
-insert into Entry_Attributes (entry_id, attribute_id) values (4, 4);
-insert into Attribute (name) values ('pede');
-insert into Attribute (name) values ('tortor');
-insert into Attribute (name) values ('urna');
-insert into Attribute (name) values ('donec');
-insert into Comment (comment, time_stamp, entry_id) values ('pellentesque', '2008-02-14 22:37', 1);
-insert into Comment (comment, time_stamp, entry_id) values ('felis', '2008-02-14 7:12', 2);
-insert into Comment (comment, time_stamp, entry_id) values ('habitasse platea dictumst', '2008-02-14 14:29', 3);
-insert into Comment (comment, time_stamp, entry_id) values ('dignissim', '2008-02-14 20:41', 4);
-insert into Dining_Hall (name) values ('a');
-insert into Dining_Hall (name) values ('b');
-insert into DiningHall_Station (dh_id, station_id) values (1, 1);
-insert into DiningHall_Station (dh_id, station_id) values (1, 2);
-insert into DiningHall_Station (dh_id, station_id) values (1, 3);
-insert into DiningHall_Station (dh_id, station_id) values (1, 4);
-insert into DiningHall_Station (dh_id, station_id) values (1, 5);
-insert into DiningHall_Station (dh_id, station_id) values (1, 6);
-insert into DiningHall_Station (dh_id, station_id) values (1, 7);
-insert into DiningHall_Station (dh_id, station_id) values (1, 8);
-insert into DiningHall_Station (dh_id, station_id) values (1, 9);
-insert into DiningHall_Station (dh_id, station_id) values (1, 10);
-insert into DiningHall_Station (dh_id, station_id) values (1, 11);
-insert into DiningHall_Station (dh_id, station_id) values (1, 12);
-insert into DiningHall_Station (dh_id, station_id) values (2, 1);
-insert into DiningHall_Station (dh_id, station_id) values (2, 2);
-insert into DiningHall_Station (dh_id, station_id) values (2, 3);
-insert into DiningHall_Station (dh_id, station_id) values (2, 4);
-insert into DiningHall_Station (dh_id, station_id) values (2, 5);
-insert into DiningHall_Station (dh_id, station_id) values (2, 6);
-insert into DiningHall_Station (dh_id, station_id) values (2, 7);
-insert into DiningHall_Station (dh_id, station_id) values (2, 8);
-insert into DiningHall_Station (dh_id, station_id) values (2, 9);
-insert into DiningHall_Station (dh_id, station_id) values (2, 10);
-insert into DiningHall_Station (dh_id, station_id) values (2, 11);
-insert into DiningHall_Station (dh_id, station_id) values (2, 12);
-insert into Station (name) values ('phasellus');
-insert into Station (name) values ('eget');
-insert into Station (name) values ('proin');
-insert into Station (name) values ('sed');
-insert into Station (name) values ('nisl');
-insert into Station (name) values ('in');
-insert into Station (name) values ('faucibus');
-insert into Station (name) values ('et');
-insert into Station (name) values ('eget');
-insert into Station (name) values ('diam');
-insert into Station (name) values ('nulla');
-insert into Station (name) values ('mauris');
+  $sql = "INSERT IGNORE into Attribute (attribute_id, name) values (1, 'Hot');
+  INSERT IGNORE into Attribute (attribute_id, name) values (2, 'Cold');
+  INSERT IGNORE into Attribute (attribute_id, name) values (3, 'Vegetarian');
+  INSERT IGNORE into Attribute (attribute_id, name) values (4, 'Vegan');
+  INSERT IGNORE into Station (station_id, name) values (1, 'Bakery');
+  INSERT IGNORE into Station (station_id, name) values (2, 'Griz');
+  INSERT IGNORE into Station (station_id, name) values (3, 'Bakery');
+  INSERT IGNORE into Station (station_id, name) values (4, 'Grill');
+  INSERT IGNORE into Station (station_id, name) values (5, 'Pizza');
+  INSERT IGNORE into Station (station_id, name) values (6, 'Deli');
+  INSERT IGNORE into Station (station_id, name) values (7, 'Home Zone');
+  INSERT IGNORE into Station (station_id, name) values (8, 'Mongolian Grill');
+  INSERT IGNORE into Station (station_id, name) values (9, 'Produce');
+  INSERT IGNORE into Station (station_id, name) values (10, 'Soup');
+  INSERT IGNORE into Station (station_id, name) values (11, 'Tex Mex');
+  INSERT IGNORE into Station (station_id, name) values (12, 'Health On The Hilltop');
+  INSERT IGNORE into Station (station_id, name) values (13, 'International');
+  INSERT IGNORE into Station (station_id, name) values (14 'Salad Bar');
+  INSERT IGNORE into Dining_Hall (dh_id, name) values (15, 'Arnold');
+  INSERT IGNORE into Dining_Hall (dh_id, name) values (16, 'Umph');
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (1, 1);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (1, 2);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (1, 3);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (1, 4);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (1, 5);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (1, 7);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (1, 8);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (1, 10);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (1, 11);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (1, 12);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (2, 1);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (2, 2);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (2, 3);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (2, 4);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (2, 5);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (2, 6);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (2, 7);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (2, 8);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (2, 9);
+  INSERT IGNORE into DiningHall_Station (dh_id, station_id) values (2, 12);";
 
-";
+  $conn->query($sql);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $sql = "INSERT into Entry (title, votes, time_stamp, image, dh_id, station_id) values ('aliquet', 76, '2010-07-14 19:00', 'http:/dummyimage.com/183x191.gif/ff4444/ffffff', 1, 4);
+  INSERT into Entry (title, votes, time_stamp, image, dh_id,  station_id) values ('purus', 38, '2008-05-14 20:00', 'http:/dummyimage.com/184x163.png/cc0000/ffffff', 2, 12);
+  INSERT into Entry (title, votes, time_stamp, image, dh_id, station_id) values ('in', 91, '2012-05-14 08:00', 'http:/dummyimage.com/158x103.jpg/5fa2dd/ffffff', 1, 1);
+  INSERT into Entry (title, votes, time_stamp, image, dh_id, station_id) values ('etiam', 61, '2009-12-14 17:00', 'http:/dummyimage.com/159x243.png/cc0000/ffffff', 2, 5);
+  INSERT into Entry_Attributes (entry_id, attribute_id) values (1, 1);
+  INSERT into Entry_Attributes (entry_id, attribute_id) values (2, 1);
+  INSERT into Entry_Attributes (entry_id, attribute_id) values (3, 2);
+  INSERT into Entry_Attributes (entry_id, attribute_id) values (4, 2);
+  INSERT into Entry_Attributes (entry_id, attribute_id) values (2, 3);
+  INSERT into Entry_Attributes (entry_id, attribute_id) values (3, 3);
+  INSERT into Entry_Attributes (entry_id, attribute_id) values (1, 4);
+  INSERT into Entry_Attributes (entry_id, attribute_id) values (4, 4);
+  INSERT into Comment (comment, time_stamp, entry_id) values ('pellentesque', '2008-02-14 22:37', 1);
+  INSERT into Comment (comment, time_stamp, entry_id) values ('felis', '2008-02-14 7:12', 2);
+  INSERT into Comment (comment, time_stamp, entry_id) values ('habitasse platea dictumst', '2008-02-14 14:29', 3);
+  INSERT into Comment (comment, time_stamp, entry_id) values ('dignissim', '2008-02-14 20:41', 4);";
   $conn->query($sql);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
