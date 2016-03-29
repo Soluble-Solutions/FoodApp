6c79a86819b375e6f86a08727c50b93a8ceb9ae2
EttaMariG <ettamari@hotmail.com>
2016-03-29 17:53:40 -0500

    right routes this time

--- routes.php
+++ routes.php
@@ -84,12 +84,14 @@ $app->put('/index',function($request,$response,$args)
 $app->post('/entry',function($request,$response,$args)
 {
   $db = $this->dbConn;
-  $dh_id = $request->getAttribute('dh_id');//??
-  $station_id = $request->getAttribute('station_id');//??
-  $attribute_id = $request->getAttribute('attribute_id');//NEED TO GET A JSON OBJ
-  $image = $request->getAttribute('image');
-  $title = $request->getAttribute('title');
-  $comment = $request->getAttribute('comment');
+  $data = $request->getParsedBody();
+  $dh_id = $data['dh_id'];
+  $station_id = $data['station_id'];//??
+  $attribute_id =$data['attribute_id'];//NEED TO GET A JSON OBJ
+  $image = $data['image'];
+  $title = $data['title'];
+  $comment = $data['comment'];
+
   $sql = "INSERT INTO Entry (image,title,time_stamp,dh_id,station_id) VALUES ('$image','$title',now(),'$dh_id','$station_id');
   INSERT INTO DiningHall_Station (dh_id,station_id) VALUES ('$dh_id','$station_id'); ";
 
@@ -103,22 +105,23 @@ $app->post('/entry',function($request,$response,$args)
 
   $entry_id = $db->query($sql);
 
-  if(!empty($comment))
+  /*if(!empty($comment))
   {
     $sql ="INSERT INTO Comment (comment,time_stamp) VALUES ('$comment',now());";
     $q = $db->query($sql);
   }
 
-  $attributes = json_decode($attribute_id, TRUE);
+  #$attributes = json_decode($attribute_id, TRUE);
+
 
   #foreach($attribute_id as $attribute)
-  for($i=0; $i<count($attributes); $i++)
+  for($i=0; $i<count($attribute_id['attribute']); $i++)
   {
-    $sql = "INSERT INTO Attributes (attribute_id) VALUES ('$attributes[$i]');
-    INSERT INTO Entry_Attributes(entry_id,attribute_id) VALUES ('$entry_id','$attributes[$i]');";
+    $sql = "INSERT INTO Attributes (attribute_id) VALUES ('$attribute_id['attribute'][$i]');
+    INSERT INTO Entry_Attributes(entry_id,attribute_id) VALUES ('$entry_id','$attributes_id['attribute'][$i]');";
     $db->query($sql);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
+  }*/
 
 });
 $app->get('/comment/{entry_id}', function ($request, $response, $args) {
