d8fba98458b173787460a4f33f911edd4264fe33
EttaMariG <ettamari@hotmail.com>
2016-04-01 17:23:28 -0500

    Entry comment working

--- routes.php
+++ routes.php
@@ -5,52 +5,29 @@ $username = "admin";
 $password = "tester123";
 $dbname = "foodapp";
 
-//$app = new \Slim\App(); //
-//$app->get('/[{name}]', function ($request, $response, $args)
 $app->get('/index', function ($request, $response, $args) {
     // Sample log message
   try{
     $sql = 'SELECT *
             FROM Entry'; #ORDER BY votes DESC
     $db = $this->dbConn;
-
     $q = $db->query($sql);
-    $check = $q->setFetchMode(PDO::FETCH_ASSOC);
-    /*while($r = $q->fetch()){
-      echo
-    }*/
-    //$row = $q->fetchAll(); //
-    $returnArr = array();
+    $check = $q->fetchAll(PDO::FETCH_ASSOC);
+    return $response->write(json_encode($check));
+    /*$returnArr = array();
     foreach($q as $row){
-      //$returnArr['success'] ='True';
-      //$data = $row[$i];
-      $returnArr[$row['entry_id']] = $row['entry_id'];
-      $returnArr[$row['title']] = $row['title'];
-      //$returnArr['votes'] = $data['votes'];
-      //$returnArr['time_stamp'] = $data['time_stamp'];
-      //$returnArr['image'] = $data['image'];
-      //$returnArr['dh_id'] = $data['dh_id'];
-      //$returnArr['station_id'] = $data['station_id'];
-    }
-    /*if(sizeof($row)>=1){
-      //$returnArr['success'] ='True';
-      $data = $row[1];
-      $returnArr['entry_id'] = $data['entry_id'];
-      $returnArr['title'] = $data['title'];
-      $returnArr['votes'] = $data['votes'];
-      $returnArr['time_stamp'] = $data['time_stamp'];
-      $returnArr['image'] = $data['image'];
-      $returnArr['dh_id'] = $data['dh_id'];
-      $returnArr['station_id'] = $data['station_id'];
+      $returnArr['entry_id'] = $row['entry_id'];
+      $returnArr['title'] = $row['title'];
+      $returnArr['votes'] = $row['votes'];
+      $returnArr['time_stamp'] = $row['time_stamp'];
+      $returnArr['image'] = $row['image'];
+      $returnArr['dh_id'] = $row['dh_id'];
+      $returnArr['station_id'] = $row['station_id'];
+      echo json_encode($returnArr);*/
 
     }
-    else{
-      $returnArr['success'] ='False';
-    }*/
-    $response->getBody()->write(json_encode($returnArr));
-    return $response;
-    //return $response->getBody()->write(json_encode($q));
-  }
+
+  #}
     /*
     if($check){
       //$response->setStatus(200);
@@ -61,26 +38,25 @@ $app->get('/index', function ($request, $response, $args) {
     } else{
       throw new PDOException('No records found.');
     }
-
   }*/
-    catch(PDOException $e){
-      $this->notFoundHandler; //404
-      //$app->$response->setStatus(404);
-      //echo "Error: ".$e.getMessage();
-    }
+  catch(PDOException $e){
+    $this->notFoundHandler; //404
+    //$app->$response->setStatus(404);
+    //echo "Error: ".$e.getMessage();
+  }
 });
+
 $app->put('/index',function($request,$response,$args)
 {#CHANGE TO RECEIVE TWO PARAMETERS
   $db = $this->dbConn;
-  $entry_id = $request->getAttribute('entry_id');
-  //$votes = $request->getAttribute('votes');
-  $sql = "UPDATE Entry SET votes = votes + 1 WHERE entry_id = '$entry_id'";
+  $data = $request->getParsedBody();
+  $entry_id = $data['entry_id'];
+  $votes = $data['votes'];
+  $sql = "UPDATE Entry SET votes = '$votes' WHERE entry_id = '$entry_id'";
   $db->query($sql);
-  /* try{
-    $sql = '';
-  }
-  */
+
 });
+
 $app->post('/entry',function($request,$response,$args)
 {
   $db = $this->dbConn;
@@ -91,9 +67,10 @@ $app->post('/entry',function($request,$response,$args)
   $image = $data['image'];
   $title = $data['title'];
   $comment = $data['comment'];
+  $time_stamp = date("Y-m-d H:i:s");
+  #$active = 1;
 
-  $sql = "INSERT INTO Entry (image,title,time_stamp,dh_id,station_id) VALUES ('$image','$title',now(),'$dh_id','$station_id');
-  INSERT INTO DiningHall_Station (dh_id,station_id) VALUES ('$dh_id','$station_id'); ";
+  $sql = "INSERT INTO Entry (image,title,time_stamp,dh_id,station_id) VALUES ('$image','$title','$time_stamp','$dh_id','$station_id');";
 
   $db->query($sql);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -101,63 +78,43 @@ $app->post('/entry',function($request,$response,$args)
   #GET Entry_id from first line (based on image)
   $sql = "SELECT entry_id
           FROM Entry
-          WHERE image = '$image';";
+          WHERE image = '$image' AND time_stamp = '$time_stamp';";
+  $query = $db->query($sql);
+  $arr = $query->fetch(PDO::FETCH_ASSOC);
+  $entry_id = $arr['entry_id'];
 
-  $entry_id = $db->query($sql);
-
-  /*if(!empty($comment))
+  if(!empty($comment))
   {
-    $sql ="INSERT INTO Comment (comment,time_stamp) VALUES ('$comment',now());";
-    $q = $db->query($sql);
-  }
-
-  #$attributes = json_decode($attribute_id, TRUE);
-
-
-  #foreach($attribute_id as $attribute)
-  for($i=0; $i<count($attribute_id['attribute']); $i++)
-  {
-    $sql = "INSERT INTO Attributes (attribute_id) VALUES ('$attribute_id['attribute'][$i]');
-    INSERT INTO Entry_Attributes(entry_id,attribute_id) VALUES ('$entry_id','$attributes_id['attribute'][$i]');";
+    $sql ="INSERT INTO Comment (comment,time_stamp,entry_id) VALUES ('$comment',now(),$entry_id);";
     $db->query($sql);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }*/
+  }
+
+  foreach($attribute_id as $attribute)
+  #for($i=0; $i<count($attribute_id['attribute']); $i++)
+  {
+    echo json_encode($attribute);
+    /*$sql = "INSERT INTO Attributes (attribute_id) VALUES (int('$attribute'));
+    INSERT INTO Entry_Attributes(entry_id,attribute_id) VALUES ('$entry_id','$attribute');";
+    $db->query($sql);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);*/
+  }
 
 });
+
 $app->get('/comment/{entry_id}', function ($request, $response, $args) {
   try{
     $entry_id = $request->getAttribute('entry_id');
-    $sql = "SELECT e.image,e.votes,a.name #ADD COMMENTS AND USE SORT BY
-            FROM Entry e
-            INNER JOIN Entry_Attributes ea
-            ON e.entry_id = '$entry_id'
-            INNER JOIN Attribute a
-            ON ea.attribute_id = a.attribute_id
+    $sql = "SELECT c.comment
+            FROM Comment c
+            INNER JOIN Entry e
+            ON e.entry_id = c.entry_id
+            AND e.entry_id = '$entry_id'
             ;";
-    /*$sql = 'SELECT e.image,e.votes,a.name
-            FROM Entry e
-            INNER JOIN Entry_Attributes ea
-            ON e.entry_id = ea.entry_id
-            INNER JOIN Attribute a
-            ON ea.attribute_id = a.attribute_id
-            UNION
-            SELECT c.comment
-            FROM Entry e
-            INNER JOIN Comment c
-            ON e.comment_id = c.comment_id
-            ;';*/
     $db = $this->dbConn;
-
     $q = $db->query($sql);
-    //$check = $q->setFetchMode(PDO::FETCH_ASSOC);
-    $row = $q->fetchAll();
-    $returnArr = array();
-    $data = $row[0];
-    $returnArr['image'] = $data['image'];
-    $returnArr['votes'] = $data['votes'];
-    $returnArr['name'] = $data['name'];
-    $response->getBody()->write(json_encode($returnArr));
-    return $response;
+    $check = $q->fetchAll(PDO::FETCH_ASSOC);
+    return $response->write(json_encode($check));
   }
   catch(PDOException $e){
     $this->notFoundHandler; //404
@@ -165,39 +122,18 @@ $app->get('/comment/{entry_id}', function ($request, $response, $args) {
     //echo "Error: ".$e.getMessage();
   }
 });
+
 $app->post('/comment',function($request,$response,$args){
   $db = $this->dbConn;
-  $time_stamp = $request->getAttribute('time_stamp'); #REMOVE
-  $comment = $request->getAttribute('comment');
-  $entry_id = $request->getAttribute('entry_id');
-  $sql = "INSERT INTO Comment (comment,time_stamp,entry_id) VALUES ('$comment','$time_stamp','$entry_id');"; #now()
-  $db->query($sql);
+  $data = $request->getParsedBody();
+  $entry_id = $data['entry_id'];
+  $comment = $data['comment'];
 
+  $sql = "INSERT INTO Comment (comment,time_stamp,entry_id) VALUES ('$comment',now(),'$entry_id');"; #now()
+  $db->query($sql);
 });
+
 $app->post('/tags',function($request,$response,$args)
 {
   #return a JSON object of all the IDs
 });
-
-/*$app->get('/goodbye', function($request, $response, $args){
-  return $response->write("Time to go. Goodbye!");
-});
-$app->get('/comments', function($request, $response, $args){
-    // Sample log message
-    //$this->logger->info("Slim-Skeleton '/' route");
-    $db = $this->dbConn;
-    $strToReturn ='';
-    //$sql = 'SELECT * FROM foodpost';
-    //$result = $db->query('SELECT * FROM foodpost');
-    //while ($row = $result->fetch_assoc()){
-    //while ($row = mysql_fetch_array($result)){
-    //foreach($db->query('select * from foodpost') as $row) {
-    foreach($db->query('SELECT * FROM foodpost') as $row) {
-    	$strToReturn .= '<br />' . $row ['comment']. '<br /> '. $row['choice']. '<br/>'. $row['time'];
-    }
-    return $response->write($strToReturn);
-    // Render index view
-    //return $this->renderer->render($response, 'index.phtml', $args);
-});
-*/
-//http://192.168.56.103/practice.php
