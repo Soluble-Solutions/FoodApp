c2f3cd0193e8b3897067f5a28f1de196f933434a
Christian Green <cmgreen@smu.edu>
2016-04-18 23:02:28 -0500

    CAMERA WORKS

--- App/www/js/controllers.js
+++ App/www/js/controllers.js
@@ -224,25 +224,24 @@ angular.module('starter.controllers', ['ngAnimate'])
 })
 
 
-.controller('PostCtrl', function($scope, $http, User) {
-  $scope.takeImage = function() {
-    console.log("takeImage() called");
+.controller('PostCtrl', function($scope, $http, User, $cordovaCamera) {
+  $scope.takeImage = function () {
     var options = {
-        quality: 80,
-        destinationType: Camera.DestinationType.DATA_URL,
-        sourceType: Camera.PictureSourceType.CAMERA,
-        allowEdit: true,
-        encodingType: Camera.EncodingType.JPEG,
-        targetWidth: 250,
-        targetHeight: 250,
-        popoverOptions: CameraPopoverOptions,
-        saveToPhotoAlbum: false
+      quality: 75,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 300,
+      targetHeight: 300,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false
     };
 
-    $cordovaCamera.getPicture(options).then(function(imageData) {
-        $scope.srcImage = "data:image/jpeg;base64," + imageData;
-    }, function(err) {
-        // error
+    $cordovaCamera.getPicture(options).then(function (imageData) {
+        $scope.imgURI = "data:image/jpeg;base64," + imageData;
+    }, function (err) {
+        // An error occured. Show a message to the user
     });
   }
 
